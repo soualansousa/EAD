b2f45a9adcdb08165bf1869eba5382e86d5d1320
Alan <soualansousa@gmail.com>
2024-10-08 09:12:43 -0300

    Finalizando a home e começando a criação da pagina notícias

--- app/views.py
+++ app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "app/pages/home.html")
+
+def noticias(request):
+    return render(request, "app/pages/noticias.html")

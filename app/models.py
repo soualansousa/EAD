e2bd25fa03f75a0a2123466943cb6d1418f22e49
Alan <soualansousa@gmail.com>
2024-10-08 11:19:58 -0300

    Criação e alteração do models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Noticia(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True, editable=False)
+    Título = models.CharField(max_length=65)
+    Descrição = models.TextField(max_length=165)
+    Data_pub = models.DateTimeField(auto_now_add=True)
+    Data_mod = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.Título
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Noticia
 
 def home(request):
     return render(request, "app/pages/home.html")
 
 def noticias(request):
-    return render(request, "app/pages/noticias.html")
+    noticias = request.GET.get(Noticia)
+    return render(request, "app/pages/noticias.html", {'noticias': noticias})

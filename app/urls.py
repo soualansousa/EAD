f04c1686cb288174f93ee5e4efcbcf01eadd62fb
Alan <soualansousa@gmail.com>
2024-10-31 15:37:22 -0300

    tela de login

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,8 @@ from . import views
 app_name= 'app'
 
 urlpatterns = [
-    path('',views.home ),
-    path('noticias/',views.noticias ),
+    path('',views.user_login, name="login"),
+    path('home_cead/',views.home_cead, name="home_cead"),
+    path('home_coo/',views.home_coo, name="home_coo"),
+    path('noticias/',views.noticias),
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from .models import Noticia
 
-def home(request):
-    return render(request, "app/pages/home.html")
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            if form.get_user().is_superuser:
+                return redirect("app:home_cead")
+            return redirect("app:home_coo")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'app/pages/login.html', {'form': form})
+
+@login_required
+def home_cead(request):
+    return render(request, "app/pages/home_cead.html")
+
+@login_required
+def home_coo(request):
+    return render(request, "app/pages/home_coo.html")
 
 def noticias(request):
     noticias = request.GET.get(Noticia)

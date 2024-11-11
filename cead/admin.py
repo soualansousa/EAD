120246b86e08876e36c991f4666451d098409c2f
alan <soualansousa@gmail.com>
2024-11-11 09:18:51 -0300

    Atualizando models

--- cead/admin.py
+++ cead/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Noticia, Curso, Polo, Coordenador
+from .models import Noticia, Curso, Polo, Coordenador, Documentos, Disciplina, Perguntas, Contato
 
 admin.site.register(Noticia),
 admin.site.register(Curso),
 admin.site.register(Coordenador),
 admin.site.register(Polo),
+admin.site.register(Documentos),
+admin.site.register(Disciplina),
+admin.site.register(Perguntas),
+admin.site.register(Contato),
--- cead/models.py
+++ cead/models.py
@@ -1,35 +1,121 @@
 from django.db import models
 from datetime import date
+from django.utils.safestring import mark_safe
+
 
 class Curso(models.Model):
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=150)
+    sobre = models.TextField()
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.nome
 
+class Documentos(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=150)
+    descricao = models.TextField()
+    arquivo = models.FileField()
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
+
+    def Foto(self):
+        self.arquivo
+        return mark_safe('<img src="{}" height="50" />' .format(self.Imagem.url))
+
+class Disciplina(models.Model):
+    MODULO_CHOICES = [
+        ('1', '1º'),
+        ('2', '2º'),
+        ('3', '3º'),
+        ]
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=150)
+    ementa = models.TextField()
+    modulo = models.CharField(max_length=10, choices=MODULO_CHOICES)
+    ch = models.IntegerField()
+    arquivo = models.FileField()
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+class Perguntas(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    pergunta = models.CharField(max_length=255)
+    resposta = models.TextField()
+    arquivo = models.FileField()
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.pergunta
+    
+class Contato(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    telefone = models.CharField(max_length=11)
+    matricula = models.CharField(max_length=50)
+    assunto = models.CharField(max_length=255)
+    mensagem = models.TextField()
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.assunto
+
 class Coordenador(models.Model):
-    nome = models.CharField(max_length=255)
+    SITUACAO_CHOICES = [
+        ('CEAD', 'CEAD'),
+        ('COOPOLO', 'Coordenador Polo'),
+        ('COOCURSO', 'Coordenador Curso'),
+        ('COO', 'Coordenador'),
+        ]
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100, null=True)
+    telefone = models.CharField(max_length=11, null=True)
+    situacao = models.CharField(max_length=20, choices=SITUACAO_CHOICES, null=True)
+    publicacao = models.DateField(auto_now_add=True, null=True)
+    edicao = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.nome
 
 class Noticia(models.Model):
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=255)
-    descricao = models.TextField()
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=900)
+    arquivo = models.FileField()
     publicacao = models.DateField(auto_now_add=True)
-    edicao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.titulo
     
 class Polo(models.Model):
     coordenador = models.ForeignKey(Coordenador, on_delete=models.CASCADE)
-    cidade = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=100)
     longitude = models.FloatField()
     latitude = models.FloatField()
     publicacao = models.DateField(auto_now_add=True)
-    edicao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.cidade
+
+class Mediador(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100, null=True)
+    telefone = models.CharField(max_length=11, null=True)
+    formacao = models.TextField(max_length=255)
+    publicacao = models.DateField(auto_now_add=True)
+    edicao = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.cidade

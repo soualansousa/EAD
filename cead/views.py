5055df017f5b2c64a240fcf5c5c430aa42c0c56c
alan <soualansousa@gmail.com>
2024-11-11 10:33:23 -0300

    adicionando a funçao para aparecer o menu lateral

--- cead/views.py
+++ cead/views.py
@@ -272,3 +272,13 @@ def curso_lista(request):
     }
 
     return render(request, 'cead/pages/curso.html', context)
+
+def detalhar_curso(request, curso_id):
+    curso = get_object_or_404(Curso, id=curso_id)
+    dados = {
+        'nome': curso.nome,
+        'sobre': curso.sobre,
+        'publicacao': curso.publicacao.strftime('%d/%m/%Y') if curso.publicacao else "Data não disponível",
+        'edicao': curso.edicao.strftime('%d/%m/%Y') if curso.edicao else "Não editado",
+    }
+    return JsonResponse(dados)

6581cf48c6def809f3f6a328d91c4975fb0e3380
alan <soualansousa@gmail.com>
2024-11-13 10:18:53 -0300

    menu lateral funcionando e exibindo as informações de notícias relacionadas a determinado curso

--- cead/static/js/curso.js
+++ cead/static/js/curso.js
@@ -17,17 +17,36 @@ $(document).ready(function() {
         });
     });
     
-    // Botão para abrir o menu lateral com detalhes do curso
     $('.button_icon').on('click', function() {
         let cursoId = $(this).data('id'); // Corrigido let para declarar a variável
-
+    
         if (cursoId) {
             $.ajax({
                 type: 'GET',
                 url: `/detalhar_curso/${cursoId}/`,
                 success: function(response) {
-                    $('#detalheNome').text(response.nome || "Nome não disponível");
-                    $('#detalheDescricao').text(response.descricao || "Descrição não disponível");    
+                    // Exibindo informações do curso
+                    $('#detalheNome').text(response.curso.nome || "Nome não disponível");
+                    $('#detalheDescricao').text(response.curso.sobre || "Descrição não disponível");
+    
+                    // Exibindo as notícias relacionadas ao curso
+                    let noticiasHTML = '';
+                    if (response.noticias.length > 0) {
+                        response.noticias.forEach(function(noticia) {
+                            noticiasHTML += `
+                                <tr class="text-center table_body" data-id="${noticia.id}">
+                                    <td>${noticia.titulo}</td>
+                                    <td>${noticia.descricao}</td>
+                                    <td>${noticia.publicacao}</td>
+                                    <td>${noticia.edicao}</td>
+                                </tr>
+                            `;
+                        });
+                        $('#tabelaNoticias tbody').html(noticiasHTML); // Supondo que a tabela tenha um ID "tabelaNoticias"
+                    } else {
+                        $('#tabelaNoticias tbody').html('<tr><td colspan="5">Nenhuma notícia encontrada.</td></tr>');
+                    }
+    
                     $('#menuLateral').addClass('open'); // Abre o menu lateral
                 },
                 error: function() {

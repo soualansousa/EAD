cf8983d38495894f1d033e02ab5856166d7565ae
jeff2420 <jeffyynhoferreira17@gmail.com>
2024-12-05 10:37:31 -0300

    Inicialização no app de coordenador

--- coo/urls.py
+++ coo/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 app_name= 'coo'
 
 urlpatterns = [
+    # path('',views.user_login, name="login"),
     path('coordenacao/',views.coordenacao, name="coordenacao"),
+
+#notícias
+    path('noticias/',views.noticias_lista, name="noticia_lista"),
+    path('criar_noticia/', views.criar_noticia, name='criar_noticia'),
+    path('excluir_noticia/<int:noticia_id>/', views.excluir_noticia, name='excluir_noticia'),
+    path('detalhar_noticia/<int:noticia_id>/', views.detalhar_noticia, name='detalhar_noticia'),
+    path('editar_noticia/<int:noticia_id>/', views.editar_noticia, name='editar_noticia'),
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+
+
--- coo/views.py
+++ coo/views.py
@@ -4,3 +4,98 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def coordenacao(request):
     return render(request, "coo/pages/home.html")
+
+
+
+def noticias_lista(request):
+    make_noticia = NoticiaForm(request.POST)
+    search_noticia = SearchForm(request.GET)
+    query = request.GET.get('query')
+    noticia_cursos = NoticiaCurso.objects.select_related('noticia', 'curso').all()
+
+    query = request.GET.get('query', '')
+    if query == 'none':
+        query = ''
+
+    if query:
+        noticia_cursos = noticia_cursos.filter(
+            Q(curso__nome__icontains=query) | Q(noticia__titulo__icontains=query) | Q(noticia__descricao__icontains=query)
+        )
+
+    noticia_cursos_paginada = Paginator(noticia_cursos, 10)
+    p = request.GET.get("p")
+    try:
+        pagina = noticia_cursos_paginada.page(p)
+    except PageNotAnInteger:
+        pagina = noticia_cursos_paginada.page(1)
+    except EmptyPage:
+        pagina = noticia_cursos_paginada.page(1)
+
+    context = {
+        'make_noticia': make_noticia,
+        'search_noticia': search_noticia,
+        'noticia_cursos': pagina,
+        'query': query,
+    }
+
+    return render(request, 'coo/pages/noticias.html', context)
+
+def criar_noticia(request):
+    if request.method == 'POST':
+        form = NoticiaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        return JsonResponse({'success': False, 'errors': form.errors})
+
+def excluir_noticia(request, noticia_id):
+    if request.method == 'POST':
+        noticia = get_object_or_404(Noticia, id=noticia_id)
+        noticia.delete()
+        return JsonResponse({'success': True})
+    return JsonResponse({'success': False, 'error': 'Método não permitido'})
+
+def detalhar_noticia(request, noticia_id):
+    curso_id = request.GET.get('curso')
+
+    try:
+        noticia_curso = NoticiaCurso.objects.select_related('noticia', 'curso').get(
+            noticia_id=noticia_id,
+            curso_id=curso_id
+        )
+    except NoticiaCurso.DoesNotExist:
+        return JsonResponse({'error': 'Notícia ou curso não encontrados.'}, status=404)
+
+    dados = {
+        'titulo': noticia_curso.noticia.titulo,
+        'descricao': noticia_curso.noticia.descricao,
+        'arquivo': noticia_curso.noticia.arquivo.url if noticia_curso.noticia.arquivo else None,
+        'curso': noticia_curso.curso.nome if noticia_curso.curso else "Curso não informado",
+        'publicacao': noticia_curso.noticia.publicacao.strftime('%d/%m/%Y') if noticia_curso.noticia.publicacao else "Data não disponível",
+        'edicao': noticia_curso.noticia.edicao.strftime('%d/%m/%Y') if noticia_curso.noticia.edicao else "Não editado",
+    }
+    return JsonResponse(dados)
+
+def editar_noticia(request, noticia_id):
+    curso_id = request.GET.get('curso')
+    noticia_curso = NoticiaCurso.objects.filter(noticia_id=noticia_id, curso_id=curso_id).first()
+
+    if request.method == 'POST':
+        form = NoticiaForm(request.POST, instance=noticia_curso.noticia, noticia_curso=noticia_curso)
+        if form.is_valid():
+            noticia = form.save()
+            return JsonResponse({'success': True})
+        return JsonResponse({'success': False, 'errors': form.errors})
+    
+    cursos = Curso.objects.all().values('id', 'nome')
+    curso_relacionado = noticia_curso.curso.id if noticia_curso else None
+    dados = {
+        'titulo': noticia_curso.noticia.titulo,
+        'descricao': noticia_curso.noticia.descricao,
+        'arquivo': noticia_curso.noticia.arquivo.url if noticia_curso.noticia.arquivo else None,
+        'publicacao': noticia_curso.noticia.publicacao.strftime('%d/%m/%Y') if noticia_curso.noticia.publicacao else "Data não disponível",
+        'edicao': noticia_curso.noticia.edicao.strftime('%d/%m/%Y') if noticia_curso.noticia.edicao else "Não editado",
+        'curso': curso_relacionado,
+        'cursos': list(cursos)
+    }
+    return JsonResponse(dados)

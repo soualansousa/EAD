98f383be376d194f860da03d42be884f3b2a52f5
alan <soualansousa@gmail.com>
2024-12-11 10:24:23 -0300

    estilizando a home

--- coo/urls.py
+++ coo/urls.py
@@ -17,14 +17,6 @@ urlpatterns = [
     path('noticias/detalhar_noticia/<int:noticia_id>/', views.detalhar_noticia, name='detalhar_noticia'),
     path('noticias/editar_noticia/<int:noticia_id>/', views.editar_noticia, name='editar_noticia'),
 
-#mediadores
-    path('mediadores/',views.mediadores_lista, name="mediador_lista"),
-    path('criar_mediador/', views.criar_mediador, name='criar_mediador'),
-    path('vincular_mediador/', views.mediacao, name='vincular_mediador'),
-    path('mediadores/excluir_mediador/<int:mediador_id>/', views.excluir_mediador, name='excluir_mediador'),
-    path('mediadores/detalhar_mediador/<int:mediador_id>/', views.detalhar_mediador, name='detalhar_mediador'),
-    path('mediadores/editar_mediador/<int:mediador_id>/', views.editar_mediador, name='editar_mediador'),
-
 #disciplina
     path('disciplina/',views.disciplina_lista, name="disciplina_lista"),
     path('criar_disciplina/', views.criar_disciplina, name='criar_disciplina'),
